f751dd7d6103b2182405b078a7f0a278f0f1f1dc
Amit Kumar Agrawal <amit.agrawal.hyd@outlook.com>
2020-09-06 22:49:33 +0530

    Coderbyte challange

---
+++ CoderbytePractice/Challenges/LongestWord.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace CoderbytePractice.Challenges
+{
+    public class LongestWord
+    {
+        private string _sentence;
+
+        public LongestWord(string sentence)
+        {
+            _sentence = sentence;
+        }
+
+        public LongestWord()
+        {
+            _sentence = "Simplicity is soul of efficiency";
+        }
+
+        public string GetLongestWordSolution0Result(string sentence = null)
+        {
+            _sentence = sentence ?? _sentence;
+
+            string result = string.Empty;
+            int longestWordLength = 0;
+
+            var words = _sentence?.Split(" ");
+
+            for(int index = 0; index < words?.Length; index++)
+            {
+                var word = RemoveSpecialCharacters(words[index]);
+                if (longestWordLength < word.Length)
+                {
+                    result = word;
+                    longestWordLength = word.Length;
+                }
+            }
+
+            return result;
+        }
+
+        private string RemoveSpecialCharacters(string word)
+        {
+            StringBuilder wordWithOutSpecialCharacters = new StringBuilder();
+
+            foreach (char c in word)
+            {
+                if (char.IsLetter(c) || char.IsDigit(c))
+                    wordWithOutSpecialCharacters.Append(c);
+            }
+
+            return wordWithOutSpecialCharacters.ToString();
+        }
+    }
+}
---
+++ CoderbytePractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using CoderbytePractice.Challenges;
+using System;
+
+namespace CoderbytePractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            LongestWord();
+        }
+
+        private static void LongestWord()
+        {
+            var longestWordChallenge = new LongestWord("Simplicity is soul of efficiency");
+
+            Console.WriteLine($"GetLongestWordSolution0Result is {longestWordChallenge.GetLongestWordSolution0Result()}");
+        }
+    }
+}
